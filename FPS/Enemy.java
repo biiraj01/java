91b22b3a0733779fce9490e15f238847a3cbe0fe
Biraj <108848282+biiraj01@users.noreply.github.com>
2024-06-16 10:25:25 +0545

    Add files via upload

---
+++ FPS/Enemy.java
@@ -0,0 +1,31 @@
+import java.awt.*;
+import java.io.File;
+import java.util.Random;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+
+public class Enemy extends JPanel {
+    Random randomNumber = new Random();
+    int x = randomNumber.nextInt(500);
+    int y = randomNumber.nextInt(200);
+    Image image;
+
+    Enemy() {
+        try {
+            image = ImageIO.read(new File("./Asteroid.png"));
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.setColor(Color.RED);
+        g.drawImage(image, x, y, 50, 050, this);
+    }
+
+    public void update() {
+        y = y + 1;
+    }
+}
